ff2b275b99a5dd170b48e2251b1e918eb67df986
Kufreabasi <engineer_kufre@yahoo.com>
2020-09-21 16:20:18 +0100

    created database getting started

---
+++ CoreApiApp/Migrations/20200921150117_NoPasswordMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreApiApp.Migrations
+{
+    public partial class NoPasswordMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ CoreApiApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreApiApp.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(30, ErrorMessage = "Entry is longer than 30 characters")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "Entry is longer than 30 characters")]
+        public string LastName { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+    }
+}
